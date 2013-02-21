61c01f0aed833e9ee7048fec149c888175e80dc2
Borisbalde <malkoy@yandex.ru>
2013-02-21 15:50:54 -0800

    Fist Submission
    Initial files to work with

---
+++ MobileIndustryCast/MobileIndustryCast/src/com/mobileindustrycast/ChatRoom.java
@@ -0,0 +1,22 @@
+package com.mobileindustrycast;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ChatRoom extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chat_room);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_chat_room, menu);
+		return true;
+	}
+
+}
