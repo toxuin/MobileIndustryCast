3f3820e6835acd5093e70b2182aaeb8c298ef6b0
Borisbalde <malkoy@yandex.ru>
2013-02-21 16:51:31 -0800

    Basic Interface

--- MobileIndustryCast/MobileIndustryCast/gen/com/mobileindustrycast/R.java
+++ MobileIndustryCast/MobileIndustryCast/gen/com/mobileindustrycast/R.java
@@ -14,15 +14,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int email=0x7f070003;
-        public static final int login=0x7f070005;
-        public static final int login_form=0x7f070002;
-        public static final int login_status=0x7f070000;
-        public static final int login_status_message=0x7f070001;
-        public static final int menu_forgot_password=0x7f070008;
-        public static final int menu_settings=0x7f070007;
-        public static final int password=0x7f070004;
-        public static final int sign_in_button=0x7f070006;
+        public static final int editText1=0x7f070002;
+        public static final int email=0x7f070010;
+        public static final int linearLayout1=0x7f070000;
+        public static final int listView1=0x7f07000c;
+        public static final int login=0x7f070012;
+        public static final int login_form=0x7f07000f;
+        public static final int login_status=0x7f07000d;
+        public static final int login_status_message=0x7f07000e;
+        public static final int menu_forgot_password=0x7f070015;
+        public static final int menu_settings=0x7f070014;
+        public static final int password=0x7f070011;
+        public static final int post_btn=0x7f07000b;
+        public static final int post_text=0x7f07000a;
+        public static final int sign_in_button=0x7f070013;
+        public static final int textView1=0x7f070001;
+        public static final int textView2=0x7f070003;
+        public static final int textView3=0x7f070008;
+        public static final int textView4=0x7f070009;
+        public static final int toggleButton1=0x7f070004;
+        public static final int toggleButton2=0x7f070005;
+        public static final int toggleButton3=0x7f070006;
+        public static final int toggleButton4=0x7f070007;
     }
     public static final class layout {
         public static final int activity_chat_room=0x7f030000;
@@ -33,21 +46,28 @@ public final class R {
         public static final int activity_login=0x7f060001;
     }
     public static final class string {
-        public static final int action_sign_in_register=0x7f040006;
-        public static final int action_sign_in_short=0x7f040007;
+        public static final int action_sign_in_register=0x7f04000d;
+        public static final int action_sign_in_short=0x7f04000e;
         public static final int app_name=0x7f040000;
-        public static final int error_field_required=0x7f04000d;
-        public static final int error_incorrect_password=0x7f04000c;
-        public static final int error_invalid_email=0x7f04000a;
-        public static final int error_invalid_password=0x7f04000b;
+        public static final int error_field_required=0x7f040014;
+        public static final int error_incorrect_password=0x7f040013;
+        public static final int error_invalid_email=0x7f040011;
+        public static final int error_invalid_password=0x7f040012;
+        public static final int filter_posts=0x7f040006;
         public static final int hello_world=0x7f040001;
-        public static final int login_progress_signing_in=0x7f040009;
-        public static final int menu_forgot_password=0x7f040008;
+        public static final int login_progress_signing_in=0x7f040010;
+        public static final int menu_forgot_password=0x7f04000f;
         public static final int menu_settings=0x7f040002;
+        public static final int new_post=0x7f040008;
+        public static final int post_button=0x7f04000a;
+        public static final int post_text_hint=0x7f040009;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f040004;
-        public static final int prompt_password=0x7f040005;
+        public static final int prompt_email=0x7f04000b;
+        public static final int prompt_password=0x7f04000c;
+        public static final int saved_alerts=0x7f040007;
+        public static final int search_hint=0x7f040005;
+        public static final int search_posts=0x7f040004;
         public static final int title_activity_chat_room=0x7f040003;
     }
     public static final class style {
