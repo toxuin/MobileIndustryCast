9adcfa69254c28575a3e8cca64edc7dbfbfe0e2a
Borisbalde <malkoy@yandex.ru>
2013-03-07 22:14:45 -0800

    fully functional adapter+nice messages
    changed clasees big time, dont change them back, that's how it supposed
    to be

--- MobileIndustryCast/MobileIndustryCast/src/com/mobileindustrycast/ChatRoom.java
+++ MobileIndustryCast/MobileIndustryCast/src/com/mobileindustrycast/ChatRoom.java
@@ -1,50 +1,78 @@
 
 package com.mobileindustrycast;
 
+import java.util.ArrayList;
+
+import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smackx.muc.MultiUserChat;
 
 import com.mobileindustrycast.R;
 import com.mobileindustrycast.XMPP_setting;
-import com.mobileindustrycast.ChatListActivity;
 
+
+import android.text.TextWatcher;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.view.View.OnClickListener;
 
 public class ChatRoom extends Activity  {
 
 	
 	XMPP_setting xmpp = new XMPP_setting();
-
+	MultiUserChat muc; 
+	Message message;
+	ArrayList<String> messages = new ArrayList<String>();
+	private Handler mHandler = new Handler();
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_room);       
         Button button = (Button) findViewById(R.id.post_btn);
-		ChatListActivity chatActivity = new ChatListActivity();
-
-		
+					
 		//Working Thread that's associated with networking function login()
 		new Thread(new Runnable() {
 			public void run() {
-				
 				try 
 				{
+					xmpp.connect();
 					xmpp.login();
-
 				} catch (XMPPException ex) {
 					System.out.println(ex.toString());
 				}
-				// xmpp.disconnect();
+				
+				//Setting up message listener
+				muc  = new MultiUserChat(xmpp.connection, xmpp.RECIPIENT);
+			    try{
+				muc.join(xmpp.USERNAME);//To include preferred user name(constrains may apply)
+		        ConsumerMUCMessageListener listener = new ConsumerMUCMessageListener();
+		        muc.addMessageListener(listener);
+			    } catch (XMPPException ex) {
+					System.out.println(ex.toString());
+				}
+		        
+		        //sending presence packet, showing your availability to the server
+				Presence presence = new Presence(Presence.Type.available);
+				xmpp.connection.sendPacket(presence);
 
 			}
 		}).start();
+		
+        createListAdapter();
 
 		//Post button event. Sends message with user defined body to the Openfire Server
 		button.setOnClickListener(new OnClickListener() {
@@ -56,7 +84,8 @@ public class ChatRoom extends Activity  {
 
 				try 
 				{
-					xmpp.sendMessage(message); 
+					sendMessage(xmpp.extendedMessage(message)); 
+					((EditText) findViewById(R.id.post_text)).setText("");
 				} catch (XMPPException e) {
 					System.out.println(e.toString());
 				}
@@ -64,8 +93,20 @@ public class ChatRoom extends Activity  {
 			}
 		});
 
-	}
+	
+    //base for filtering using search
+        EditText searchWords = (EditText) findViewById(R.id.search_text);
+        searchWords.addTextChangedListener(new TextWatcher() {
 
+        public void afterTextChanged(Editable s) {}
+
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+		public void onTextChanged(CharSequence s, int start,int before, int count) {
+			
+		}
+		});}
+            
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //Inflate the menu; this adds items to the action bar if it is present.
@@ -73,7 +114,57 @@ public class ChatRoom extends Activity  {
         return true;
     }
     
-	
-   
+    public void createListAdapter()
+    {
+    	final ListView mlist = (ListView) findViewById(R.id.messagesListView);
+    	mlist.setTranscriptMode(ListView.TRANSCRIPT_MODE_NORMAL);
+    	mlist.setStackFromBottom(true);
+	    ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, messages);
+		mlist.setAdapter(adapter);
+		adapter.notifyDataSetChanged();
+
+    }
+
+  //sending message to the Multi-User Chat
+    public void sendMessage(String message_body) throws XMPPException {
+    	System.out.println("XMPP_setting class' function 'sendMessage' started");
+        
+            try
+            {
+                muc.sendMessage(message_body);
+            }
+            catch (XMPPException e)
+            {
+                System.out.println(e.getMessage());
+            }
+
+
+    }
+    
+    //Multi-User Chat listener class, necessary for packet(message type in this case) retrieval from the server. Adds messages received to the "messages" array
+    class ConsumerMUCMessageListener implements PacketListener {
+ 
+
+		public void processPacket(Packet packet) {
+            if ( packet instanceof Message) {
+            	message = (Message) packet;
+            	
+   				messages.add(message.getBody());
+                System.out.println(message.getFrom() +": " + message.getBody());
+                // Add the incoming message to the list view(crashes the application currently)
+                mHandler.post(new Runnable() {
+                    public void run() {
+                   	createListAdapter();
+                    }
+                  });
+            }
+        }
+
+    }
+    
+    protected void onDestroy()
+    {
+    	xmpp.disconnect();
+    }
 }
 
--- MobileIndustryCast/MobileIndustryCast/src/com/mobileindustrycast/XMPP_setting.java
+++ MobileIndustryCast/MobileIndustryCast/src/com/mobileindustrycast/XMPP_setting.java
@@ -2,9 +2,13 @@ package com.mobileindustrycast;
 
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,6 +40,7 @@ import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.packet.Packet;
 import org.jivesoftware.smack.util.StringUtils;
 import org.jivesoftware.smackx.muc.MultiUserChat;
+import org.jivesoftware.smackx.packet.DelayInformation;
 import org.jivesoftware.smackx.pubsub.EventElement;
 
 
@@ -46,41 +51,29 @@ import org.jivesoftware.smackx.pubsub.EventElement;
  */
 public class XMPP_setting extends Activity {
     //for final server use
-	private static final String HOST = "Velington-PC";//"http://velington-pc"
-	private static final int PORT_NUMBER = 5222 ;
-	private static final String SERVICE = "conference.velington-pc";
-	private static final String RECIPIENT = "industrycast";
-	private static final String LOGIN="andrey";
-	private static final String PASSWORD="12345";
-	
-	MultiUserChat muc; 
+	public  final String HOST = "Velington-PC";//"http://velington-pc"
+	public  final int PORT_NUMBER = 5222 ;
+	public  final String SERVICE = "conference.velington-pc";
+	public  final String RECIPIENT = "industrycast@conference.velington-pc";
+	public  final String LOGIN="andrey";
+	public  final String PASSWORD="12345";
+	public  final String USERNAME="Testbot";
 
-	private Handler mHandler = new Handler();
-	private ArrayList<String> messages = new ArrayList<String>();
-	XMPPConnection connection;
 
-	  
-	
-	
-	public void createListAdapter() {
-	
-		final ListView mlist = (ListView) findViewById(R.id.messagesListView);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, messages);
-		mlist.setAdapter(adapter);
-	}
-	
-	
-    //Set of functions necessary to perform login(from user point perspective)
-	public void login() throws XMPPException {
+	Message message;
 
+
+	XMPPConnection connection;	  
+	
 		
-		System.out.println("XMPP_Connect class' function 'login' started");
+	public void connect()
+	{	
 		
 		//Configuring connection parameters with the setting predefined by team or user's input if available
 		ConnectionConfiguration config = new ConnectionConfiguration(HOST,PORT_NUMBER, SERVICE);
 		
 		// config.setDebuggerEnabled(true); <--helpful if you are encountering a problem on the XMPP side(LogCat output)
-		System.out.println("XMPP_Connect class' function 'login' configuration compleate");
+
 
 		connection = new XMPPConnection(config);
 
@@ -92,65 +85,42 @@ public class XMPP_setting extends Activity {
 			System.out.println("Exception at connect");
 			System.out.println(ex.toString());
 		}
-		System.out.println("connection to the server established");
+	}
+	
+    //Set of functions necessary to perform login(from user point perspective)
+	public void login() throws XMPPException {
 
 		try {
 			//user login. Needs to match the data from the DB associated with OpenFire Server
-			connection.login(LOGIN, PASSWORD);
-			
-			//Setting up message listener
-			muc  = new MultiUserChat(connection, "industrycast@conference.velington-pc");
-		    muc.join("Testbot");//To include preferred user name(constrains may apply)
-	        ConsumerMUCMessageListener listener = new ConsumerMUCMessageListener();
-	        muc.addMessageListener(listener);
-			
-	        //sending presence packet, showing your availability to the server
-			Presence presence = new Presence(Presence.Type.available);
-			connection.sendPacket(presence);						
+			connection.login(LOGIN, PASSWORD);						
 		} catch (XMPPException ex) {
 			System.out.println(ex.toString());
 		}
-		System.out.println("XMPP_Connect class' function 'login' ended");
-
 	}
     
-	//sending message to the Multi-User Chat
-    public void sendMessage(String message_body) throws XMPPException {
-    	System.out.println("XMPP_Connect class' function 'sendMessage' started");
-        	 	
-            try
-            {
-                muc.sendMessage(message_body);
-            }
-            catch (XMPPException e)
-            {
-                System.out.println(e.getMessage());
-            }
-
-            messages.add(": " + message_body);
-    }
     
- 
+    //Adds Zulu timestamp and Username to the message body
+    public String extendedMessage(String message_body)
+    {
+    	Date messageDate = new Date();
+
+    	SimpleDateFormat hoursAmPm = new SimpleDateFormat("hh", Locale.US);
+    	SimpleDateFormat minutesAmPm = new SimpleDateFormat("mm", Locale.US);
+    	SimpleDateFormat lettersAmPm = new SimpleDateFormat("aa", Locale.US);
+
+    	StringBuilder hours = new StringBuilder(hoursAmPm.format(messageDate));
+    	StringBuilder minutes = new StringBuilder(minutesAmPm.format(messageDate));
+    	StringBuilder letters = new StringBuilder(lettersAmPm.format(messageDate));
+
+    	String exMessage = 	"["+hours+":"+minutes+" "+letters+"] "+ USERNAME+": "+message_body;
+    	
+    	return exMessage;
+	}
+    
     //Function used to disconnect from the server connection(may be necessary if the server doesn't kick idle users) to be used in onDestruct()
     public void disconnect() {
         connection.disconnect();
     }
-    //Multi-User Chat listener class, necessary for packet(message type in this case) retrieval from the server. Adds messages received to the "messages" array
-    class ConsumerMUCMessageListener implements PacketListener {
- 
-        public void processPacket(Packet packet) {
-            if ( packet instanceof Message) {
-            	Message msg = (Message) packet;
-            	messages.add(msg.getFrom() +": " + msg.getBody());
-                System.out.println(msg.getFrom() +": " + msg.getBody());
-                // Add the incoming message to the list view(crashes the application currently)
-                /*mHandler.post(new Runnable() {
-                    public void run() {
-                      createListAdapter();
-                    }
-                  });*/
-            }
-        }
+    
 
-    }
 }
